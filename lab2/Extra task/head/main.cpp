6d61a22530c906680656f4a98640ca6947dec61b
Onexx <ilya7538@mail.ru>
2021-11-11 19:15:47 +0300

    Finished HW 2 extra task

---
+++ lab2/Extra task/head/main.cpp
@@ -0,0 +1,71 @@
+#include <algorithm>
+#include <cstring>
+#include <fstream>
+#include <iostream>
+#include <iterator>
+#include <queue>
+#include <string>
+#include <vector>
+
+namespace {
+
+template<class C>
+void print_out(std::ostream &strm, const C &c) {
+    std::ostream_iterator<typename C::value_type> out(strm, "\n");
+    std::copy(c.begin(), c.end(), out);
+}
+
+void process_stream(std::istream &input, const int num_lines) {
+
+    std::deque<std::string> lines;
+    std::string line;
+    while (std::getline(input, line)) {
+        if (lines.size() == abs(num_lines)) {
+            if (num_lines < 0) {
+                lines.push_back(line);
+                lines.pop_front();
+            }
+        } else {
+            lines.push_back(line);
+        }
+    }
+
+    print_out(std::cout, lines);
+}
+
+}// anonymous namespace
+
+int main(int argc, char **argv) {
+    int lines = 10;
+    std::vector<const char *> input_names;
+    for (int i = 1; i < argc; ++i) {
+        if (argv[i][0] == '-') {
+            if (argv[i][1] != '-') {
+                const size_t len = std::strlen(argv[i]);
+                for (size_t j = 1; j < len; ++j) {
+                    switch (argv[i][j]) {
+                        case 'n':
+                            lines = std::stoi(argv[++i]);
+                            break;
+                    }
+                }
+            } else {
+                std::string str(argv[i]);
+                if (std::strncmp(argv[i], "--lines", 7) == 0) {
+                    lines = std::stoi(str.substr(8));
+                }
+            }
+        } else {
+            input_names.push_back(argv[i]);
+        }
+    }
+
+    if (!input_names.empty()) {
+        for (auto input_name : input_names) {
+            std::ifstream f(input_name);
+            process_stream(f, lines);
+        }
+    } else {
+        process_stream(std::cin, lines);
+    }
+}
